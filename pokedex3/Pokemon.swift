0bb8343de114c3ca1033b2dc8b59504042108912
Shin Park <shinpark@shins-mac-mini.home>
2017-03-22 08:45:41 -0500

    Created a Pokemon class

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Shin Park on 3/22/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
